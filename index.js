d5549ebaf664372f23d46385f81718cd7ac4b61a
saulo santos <saulodev09@gmail.com>
2026-01-21 17:38:11 -0300

    index.js
    Calcula o custo total de combustível de uma viagem com base na distância percorrida, consumo do veículo e preço do combustível.

---
+++ index.js
@@ -0,0 +1,6 @@
+const precoCombsutivel = 5.79;
+const kmPorLitro = 12;
+const distanciaEmKm = 1580;
+const litrosconsumidos = distanciaEmKm / kmPorLitro;
+const custotoal = litrosconsumidos * precoCombsutivel;
+console.log(custotoal.toFixed(2)); 
