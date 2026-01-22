59eea66adb1e761884efaf760628ab124d20456b
saulo santos <saulodev09@gmail.com>
2026-01-22 14:03:07 -0300

    uma calculadora de gasolina e etanol
    Projeto desenvolvido para praticar lógica de programação em JavaScript, utilizando variáveis, operações matemáticas e estruturas condicionais (if / else).

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const precoCombsutivel = 5.79;
-const kmPorLitro = 12;
-const distanciaEmKm = 1580;
-const litrosconsumidos = distanciaEmKm / kmPorLitro;
-const custotoal = litrosconsumidos * precoCombsutivel;
-console.log(custotoal.toFixed(2)); 
+const etanolPreco = 5.79;
+const etanolConsumo = 7; 
+const distancia = 100;
+
+const custoEtanol = etanolPreco * distancia;
+const etanolTotal = custoEtanol / etanolConsumo;
+
+const gasolinaPreco = 6.66;
+const gasolinaConsumo = 10; 
+
+const custoGasolina = gasolinaPreco * distancia;
+const gasolinaTotal = custoGasolina / gasolinaConsumo;
+
+console.log("Etanol:", etanolTotal.toFixed(2));
+console.log("Gasolina:", gasolinaTotal.toFixed(2));
+
+if (gasolinaTotal < etanolTotal) {
+    console.log("Melhor colocar gasolina");
+} else {
+    console.log("Melhor colocar etanol");
+}
